49988dded1c55c9f24d9f36d83ba3582a2ede3c3
Chumarin Rafael <rchumarin@gmail.com>
2015-11-23 23:16:48 +0300

    New examples Collections

---
+++ Collections/Simple examples -Collections/lesson4/src/main/java/lesson4/Main.java
@@ -0,0 +1,13 @@
+package lesson4;
+
+import java.util.ArrayList;
+
+public class Main {
+	public static void main(String[] args) {
+//		Cat cat = new Cat();
+//		ArrayList<Pet> cats = new ArrayList<Pet>();
+//		Cat cat1 = new Cat();
+//		cat1.name = "Васька";
+//		cats.add(cat1);
+	}
+}
